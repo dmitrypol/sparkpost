80f99fa9b3df0649c3dce44aa4cb8f6428004b38
Dmitry Polyakovsky <dmitry@snap-raise.com>
2015-12-07 15:06:22 -0800

    added initialize to use instance methods
    Signed-off-by: Dmitry Polyakovsky <dmitry@snap-raise.com>

--- lib/sparkpost.rb
+++ lib/sparkpost.rb
@@ -2,27 +2,32 @@ require "sparkpost/version"
 require 'rest-client'
 
 module Sparkpost
-  # Your code goes here...
-  BASE_URL = "https://api.sparkpost.com/api/v1/"
+	class API
 
-  def self.transmissions(api_key, name, email, from, subject, html, text)
-	api_url = "#{BASE_URL}#{__method__}"
+	  def initialize(api_key)
+	    @api_key = api_key
+	    @base_url = "https://api.sparkpost.com/api/v1/"
+	  end  
 
-	params = {
-		content: {
-			from: from, 
-			subject: subject, 
-			text: text,
-			html: html,
-			}, 
-		recipients: [
-			#{address: 'email1@gmail.com'},
-			#{address: {name: 'name', email: 'email1@gmail.com'}},
-			{address: {name: name, email: email}},
-			]}
-	headers = {:Authorization => api_key, :content_type => :json, :accept => :json}
-	resp = RestClient.post api_url, params.to_json, headers
+	  def transmissions(name, email, from, subject, html, text)
+		api_url = "#{@base_url}#{__method__}"
 
-  end
+		params = {
+			content: {
+				from: from, 
+				subject: subject, 
+				text: text,
+				html: html,
+				}, 
+			recipients: [
+				#{address: 'email1@gmail.com'},
+				#{address: {name: 'name', email: 'email1@gmail.com'}},
+				{address: {name: name, email: email}},
+				]}
+		headers = {:Authorization => @api_key, :content_type => :json, :accept => :json}
+		resp = RestClient.post api_url, params.to_json, headers
 
+	  end
+
+	end
 end
