8ef3441df0dadb99bbd292cce5bc8631d18920f8
Dmitry Polyakovsky <dmitry@snap-raise.com>
2015-12-08 07:23:49 -0800

    returh hash instead of json string
    Signed-off-by: Dmitry Polyakovsky <dmitry@snap-raise.com>

--- lib/sparkpost.rb
+++ lib/sparkpost.rb
@@ -10,22 +10,23 @@ module Sparkpost
 	  end  
 
 	  def transmissions(name, email, from, subject, html, text)
-		api_url = "#{@base_url}#{__method__}"
+			api_url = "#{@base_url}#{__method__}"
 
-		params = {
-			content: {
-				from: from, 
-				subject: subject, 
-				text: text,
-				html: html,
-				}, 
-			recipients: [
-				#{address: 'email1@gmail.com'},
-				#{address: {name: 'name', email: 'email1@gmail.com'}},
-				{address: {name: name, email: email}},
-				]}
-		headers = {:Authorization => @api_key, :content_type => :json, :accept => :json}
-		resp = RestClient.post api_url, params.to_json, headers
+			params = {
+				content: {
+					from: from, 
+					subject: subject, 
+					text: text,
+					html: html,
+					}, 
+				recipients: [
+					#{address: 'email1@gmail.com'},
+					#{address: {name: 'name', email: 'email1@gmail.com'}},
+					{address: {name: name, email: email}},
+					]}
+			headers = {:Authorization => @api_key, :content_type => :json, :accept => :json}
+			resp = RestClient.post api_url, params.to_json, headers
+			return JSON.parse(resp)
 
 	  end
 
